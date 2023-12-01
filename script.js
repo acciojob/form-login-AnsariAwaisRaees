b04f3cc505775edc3c66883f3e743911c8b9f779
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-12-02 00:28:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstName = document.getElementById("firstName").value;
+	let lastName = document.getElementById("lastName").value;
+	alert(firstName+" "+lastName);
 }
